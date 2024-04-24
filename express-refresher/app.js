e96e65177839728d0099518bf614f49ca547a2d1
SuzuhRevv <joaopk04@hotmail.com>
2024-04-24 17:43:58 -0300

    Setting up the express static to render all styles in the page

--- express-refresher/app.js
+++ express-refresher/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 let { people } = require('./data')
 
+app.use(express.static('./methods-public/'))
 
 app.get('/api/people', (req, res) => {
     res.status(200).json({sucess: true, data: people}) //In this case i can use only people to return all the json data in my data.js or i can just pass some name : people 
-    })
+})
+
+
 
 app.listen(5000, () => {
     console.log('listening on port 5000...');
